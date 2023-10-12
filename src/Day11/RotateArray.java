6ada54682af1a4b21cde8a97a4dbd88021a80306
Ankita Soni <ankitasoni018@gmail.com>
2023-10-12 15:56:28 +0530

    Rotate Array

--- src/Day11/RotateArray.java
+++ src/Day11/RotateArray.java
@@ -3,18 +3,18 @@ package Day11;
 public class RotateArray {
     public static void main(String[] args) {
         int nums[] = {1, 2, 3, 4, 5, 6, 7};
-        int k = 4;
+        int k = 3;
         int n = nums.length;
         k %= n;
-        int kk[] = new int[k];
-        for (int i = 0; i < k; i++) {
+        int kk[] = new int[n-k];
+        for (int i = 0; i < n-k; i++) {
             kk[i] = nums[i];
         }
-        for (int i =  k; i < n; i++) {
-            nums[i - k] = nums[i];
+        for (int i =  n-k; i < n; i++) {
+            nums[i - n + k] = nums[i];
         }
-        for (int i = n - k; i < n; i++) {
-            nums[i] = kk[i - n+k];
+        for (int i = 0; i < n-k; i++) {
+            nums[i+k] = kk[i];
         }
         for (int i = 0; i < n; i++) {
             System.out.print(nums[i] + " ");
