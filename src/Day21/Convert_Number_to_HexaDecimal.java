f100d9d606666de9e5afe2bf13e03240723435b6
Ankita Soni <ankitasoni018@gmail.com>
2023-10-21 00:41:17 +0530

    Convert Number to HexaDecimal

---
+++ src/Day21/Convert_Number_to_HexaDecimal.java
@@ -0,0 +1,8 @@
+package Day21;
+
+public class Convert_Number_to_HexaDecimal {
+    public static void main(String[] args) {
+        int num = -1;
+        System.out.println(Integer.toHexString(num));
+    }
+}
