d71960289a1173d43f03a814f6ac17183db6f0d1
Ankita Soni <ankitasoni018@gmail.com>
2023-10-27 17:01:34 +0530

    Count Integers With Even Digit Sum

--- src/Day25/Count_Digit_with_Even_DIgit_Sum.java
+++ src/Day25/Count_Digit_with_Even_DIgit_Sum.java
@@ -2,7 +2,7 @@ package Day25;
 
 public class Count_Digit_with_Even_DIgit_Sum {
     public static void main(String[] args) {
-        int num =4;
+        int num =30;
         int count = 0;
         for (int i =2; i <= num; i++){
             if (sumofdigit(i)){
@@ -12,11 +12,10 @@ public class Count_Digit_with_Even_DIgit_Sum {
         System.out.println(count);
     }
     public static boolean sumofdigit(int n){
-        String s = String.valueOf(n);
-        int digit = 0;
-        for (int i =0; i<s.length(); i++){
-            char c = s.charAt(i);
-            digit += Character.getNumericValue(c);
+        int digit =0;
+        while (n >0){
+            digit += n %10;
+            n /= 10;
         }
         return (digit % 2 == 0);
     }
