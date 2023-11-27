9b26b4a7526d40fc3e2d76abf5d787616092a215
Ankita Soni <ankitasoni018@gmail.com>
2023-11-28 00:11:25 +0530

    Greatest Common Divisor of Strings

--- src/Day56/Greatest_Common_Divisor_of_Strings.java
+++ src/Day56/Greatest_Common_Divisor_of_Strings.java
@@ -5,7 +5,8 @@ public class Greatest_Common_Divisor_of_Strings {
         String s1 = "ABABAB", s2 = "ABAB";
         int n1 = s1.length();
         int n2 = s2.length();
-        if(s1 + s2 != s2+s1){
+        String s = s1 + s2;
+        if(!s.equals(s2+s1)){
             System.out.println("");
         } else{
             System.out.println(s1.substring(0, gcd(n1,n2)));
@@ -13,12 +14,9 @@ public class Greatest_Common_Divisor_of_Strings {
 
     }
     public static int gcd(int a, int b){
-        if(a == 0 || b == 0){
-            return a^b;
+        if(b == 0){
+            return a;
         }
-        if(b > a){
-            return gcd(b,a);
-        }
-        return gcd(a%b, b);
+        return gcd(b, a%b);
     }
 }
